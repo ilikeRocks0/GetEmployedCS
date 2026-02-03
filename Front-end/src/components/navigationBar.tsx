51663d8905c45f4d60fac27379a9d3368af4e425
Ty Paragas <ty.paragas01@gmail.com>
2026-02-03 17:25:06 -0600

    [Application Management / Search Jobs] Implement Job queries * Move non-src files out of src directory * Create new directories for back-end src * Add interface for job data access class * Add implementation for job data access class * Create AppDbContext and corresponding model * Add business objects directory and Job class * Changed background to white * Added static navigation Bar * Deleted old template * Moved NavigationBar logic to its own class * Finished Implementing The Search Job Page * can now use the navigation header to traverse to the main page and header page

---
+++ Front-end/src/components/navigationBar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function NavigationBar() {
+    return (
+        <div className="flex h-20 bg-blue-200">
+            <div className="my-auto flex w-full">
+            <Link href="/" className="inline px-5 pr-0 font-bold">GetEmployed.CS</Link>
+
+            <div className="grid grid-cols-4 gap-x-10 justify-self-center">
+                <Link href="/search-jobs" className="inline text-end">Job List</Link>
+                <p className="inline">Writing Help</p>
+                <p className="inline">Manage Jobs</p>
+                <p className="inline">Add Jobs</p>
+            </div>
+
+            <div className="absolute right-3 top-2 border w-15 h-15 rounded-4xl bg-white">
+                <p className="my-4 text-center">Profile</p>    
+            </div>    
+            </div>
+        </div>
+    )    
+}
