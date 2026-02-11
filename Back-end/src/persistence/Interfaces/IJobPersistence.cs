323d3d5442fcf32fb3f791c3dcce4c3308c80978
Ty Paragas <ty.paragas01@gmail.com>
2026-02-10 18:15:30 -0600

    [Bug - Application Management / Search Jobs] Add search term filter on both queries * Also adds filtering in general for the saved jobs query * Remove logic for DateTime conversion from Persistence class * Make config instance private in JobPersistence

--- Back-end/src/persistence/Interfaces/IJobPersistence.cs
+++ Back-end/src/persistence/Interfaces/IJobPersistence.cs
@@ -4,6 +4,6 @@ namespace Back_end.Persistence.Interfaces;
 
 public interface IJobPersistence
 {
-  public List<Job> GetJobs(List<string> language, List<string> positionType, List<string> employmentType);
-  public List<Job> GetSavedJobs(int userId, string language, string positionType, string employmentType);
+  public List<Job> GetJobs(string searchTerm, List<string> languages, List<string> positionTypes, List<string> employmentTypes);
+  public List<Job> GetSavedJobs(int seekerId, string searchTerm, List<string> languages, List<string> positionTypes, List<string> employmentTypes);
 }
