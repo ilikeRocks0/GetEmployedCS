d5f27fdfca357e178494bebfc05e307fdad3faa2
Daniel Gorban <daniel@gorban.org>
2026-03-18 13:14:06 -0500

    [Resume and Cover Letter Improvement] Added endpoint and service for generic words

---
+++ Back-end/src/Services/Interfaces/IGenericWordsService.cs
@@ -0,0 +1,6 @@
+namespace Back_end.Services.Interfaces;
+interface IGenericWordsService
+{
+    /// <summary>Extracts and returns a list of what position the generic words are in the paragraph. first word is 0, second word is 1, etc.</summary>
+    List<int> GetPositionOfGenericWords(string Paragraph);
+}
